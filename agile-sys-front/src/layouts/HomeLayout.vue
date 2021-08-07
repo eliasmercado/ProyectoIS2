bcb96a4f172039159c2b91a601de7fb5729fc42d
Mario Villalba <marioruben23.mv@fpuna.edu.py>
2021-08-07 17:09:40 -0400

    Se modifica la pantalla de inicio para agregar acceso a los tres modulos del proyecto.

--- agile-sys-front/src/layouts/HomeLayout.vue
+++ agile-sys-front/src/layouts/HomeLayout.vue
@@ -1,25 +1,84 @@
 <template>
-  <div>
+  <div id="app">
     <v-app id="inspire">
-      <v-app-bar :clipped-left="true" app class="appBar" dark>
-        <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
-          <v-avatar tile>
-              <img
-                src="@/assets/logo-AS.png"
-              />
-          </v-avatar>
-          <span class="ml-2 textStyle text-h5 font-weight-bold">Agile Sys</span>
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
+      <v-app id="inspire">
+        <v-navigation-drawer v-model="drawer" app class="navBar" dark>
+          <v-list>
+            <v-list-item>
+              <v-list-item-avatar>
+                <v-img src="../assets/user-icon.png"></v-img>
+              </v-list-item-avatar>
 
-        <v-btn @click="logoutUser" icon large>
-          <v-icon>mdi-logout</v-icon>
-        </v-btn>
-      </v-app-bar>
+              <v-list-item-content>
+                <v-list-item-title class="title">
+                  {{ $store.state.LoginStore.nombres.split(" ")[0] }}
+                  {{ $store.state.LoginStore.apellidos.split(" ")[0] }}
+                </v-list-item-title>
+                <v-list-item-subtitle>{{ rolUsuario }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
 
-      <v-container>
+          <v-divider></v-divider>
+
+          <v-list shaped>
+            <v-list-item to="/">
+              <v-list-item-action>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title class="text-body-1 font-weight-medium">
+                  Inicio
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <div v-for="item in items" :key="item.id">
+              <v-subheader class="font-weight-bold">{{
+                item.title
+              }}</v-subheader>
+              <template v-for="childItem in item.childItems">
+                <v-list-item :key="childItem.text" link :to="childItem.to">
+                  <v-list-item-action>
+                    <v-icon>{{ childItem.icon }}</v-icon>
+                  </v-list-item-action>
+                  <v-list-item-content>
+                    <v-list-item-title class="text-body-2 font-weight-medium">
+                      {{ childItem.text }}
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </template>
+              <v-divider></v-divider>
+            </div>
+          </v-list>
+          <template v-slot:append>
+            <div class="pa-2">
+              <v-btn @click="logoutUser" class="primary" block>Logout</v-btn>
+            </div>
+          </template>
+        </v-navigation-drawer>
+
+        <v-toolbar flat color="appBarHomeLayout" dark>
+          <v-app-bar-nav-icon
+            @click.stop="drawer = !drawer"
+          ></v-app-bar-nav-icon>
+
+          <v-toolbar-title>
+            <v-avatar tile> <img src="@/assets/logo-AS.png" /> </v-avatar>
+            <span class="text-h5 textStyle font-weight-bold ml-2"
+              >Agile Sys</span
+            >
+          </v-toolbar-title>
+
+          <v-spacer></v-spacer>
+
+          <v-btn @click="logoutUser" icon>
+            <v-icon>mdi-logout</v-icon>
+          </v-btn>
+        </v-toolbar>
+
         <router-view></router-view>
-      </v-container>
+      </v-app>
     </v-app>
   </div>
 </template>
@@ -30,12 +89,45 @@ import { mapActions } from "vuex";
 export default {
   data: () => ({
     dialog: false,
+    drawer: null,
+    idRol: 0,
+    rolUsuario: "",
     items: [
-      { icon: "mdi-clipboard-text", text: "Backlog", to: "/backlog" },
-      { icon: "mdi-view-week-outline", text: "Tablero", to: "/board" },
-      { icon: "mdi-account-group", text: "Miembros", to: "/members" },
-      { icon: "mdi-cog", text: "Settings", to: "/settings" },
-      { icon: "mdi-help-circle", text: "Ayuda", to: "/help" },
+      {
+        id: 1,
+        title: "SEGURIDAD",
+        childItems: [
+          { icon: "mdi-account", text: "Usuarios", to: "" },
+          { icon: "mdi-account-key", text: "Permisos", to: "" },
+          { icon: "mdi-account-cog", text: "Roles", to: "" },
+          { icon: "mdi-account-plus", text: "Asignar Roles", to: "" },
+        ],
+      },
+
+      {
+        id: 2,
+        title: "PROYECTO",
+        childItems: [
+          { icon: "mdi-clipboard-edit", text: "Administrar", to: "" },
+          {
+            icon: "mdi-account-multiple-plus",
+            text: "Agregar Miembros",
+            to: "",
+          },
+        ],
+      },
+
+      {
+        id: 3,
+        title: "DESARROLLO",
+        childItems: [
+          { icon: "mdi-clipboard-text", text: "Backlog", to: "/backlog" },
+          { icon: "mdi-view-week-outline", text: "Tablero", to: "/board" },
+          { icon: "mdi-account-group", text: "Miembros", to: "/members" },
+          { icon: "mdi-cog", text: "Settings", to: "/settings" },
+          { icon: "mdi-help-circle", text: "Ayuda", to: "/help" },
+        ],
+      },
     ],
   }),
 
@@ -45,23 +137,40 @@ export default {
     logoutUser() {
       this.logout();
     },
+
+    cargarRolUsuario() {
+      let idProyecto = this.$store.state.LoginStore.idProyecto;
+      let idUsuario = this.$store.state.LoginStore.idUsuario;
+      let url = `/v1/rol-proyecto/${idProyecto}/${idUsuario}`;
+
+      this.axios
+        .get(url)
+        .then((result) => {
+          this.rolUsuario = result.data.data.descripcionRol;
+          this.idRol = result.data.data.idRol;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+
+  mounted() {
+    this.cargarRolUsuario();
   },
 };
 </script>
 
 <style lang="scss">
-.appBar {
-  background: linear-gradient(
-    90deg,
-    rgba(2, 0, 36, 1) 0%,
-    rgba(9, 9, 121, 1) 0%,
-    rgba(1, 147, 241, 1) 0%,
-    rgba(6, 79, 167, 1) 0%,
-    rgba(1, 147, 241, 1) 100%,
-    rgba(0, 212, 255, 1) 100%
-  ) !important;
+$nav-bar-color: #263238;
+$app-bar-color: #1976d2;
+.appBarHomeLayout {
+  background: $app-bar-color !important;
+  max-height: 10%;
 }
-
+.navBar {
+  background-color: $nav-bar-color !important;
+}
 .textStyle {
   font-family: Verdana, Geneva, Tahoma, sans-serif;
 }
