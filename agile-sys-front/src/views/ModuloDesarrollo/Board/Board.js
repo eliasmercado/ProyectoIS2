49ab7dec591825c282a0a0f55b28b9a5c633c845
Elias <eliasmerc23@fpuna.edu.py>
2021-10-08 20:55:58 -0300

    Kanban para la parte 2 del modulo de desarrollo.

--- agile-sys-front/src/views/ModuloDesarrollo/Board/Board.js
+++ agile-sys-front/src/views/ModuloDesarrollo/Board/Board.js
@@ -1,111 +1,94 @@
-import draggable from "vuedraggable";
-import TaskCard from "@/components/TaskCard.vue";
+import { DxScrollView } from "devextreme-vue/scroll-view";
+import { DxSortable } from "devextreme-vue/sortable";
+
 export default {
-  name: "App",
   components: {
-    TaskCard,
-    draggable,
+    DxScrollView,
+    DxSortable,
   },
-  data() {
-    return {
-      columns: [
-        {
-          title: "POR HACER",
-          tasks: [
-            {
-              id: 1,
-              title: "Add discount code to checkout page",
-              date: "Sep 14",
-              type: "Feature Request",
-            },
-            {
-              id: 2,
-              title: "Provide documentation on integrations",
-              date: "Sep 12",
-            },
-            {
-              id: 3,
-              title: "Design shopping cart dropdown",
-              date: "Sep 9",
-              type: "Design",
-            },
-            {
-              id: 4,
-              title: "Add discount code to checkout page",
-              date: "Sep 14",
-              type: "Feature Request",
-            },
-            {
-              id: 5,
-              title: "Test checkout flow",
-              date: "Sep 15",
-              type: "QA",
-            },
-          ],
-        },
-        {
-          title: "EN CURSO",
-          tasks: [
-            {
-              id: 9,
-              title: "Provide documentation on integrations",
-              date: "Sep 12",
-            },
-            {
-              id: 10,
-              title: "Design shopping cart dropdown",
-              date: "Sep 9",
-              type: "Design",
-            },
-            {
-              id: 11,
-              title: "Add discount code to checkout page",
-              date: "Sep 14",
-              type: "Feature Request",
-            },
-            {
-              id: 12,
-              title: "Design shopping cart dropdown",
-              date: "Sep 9",
-              type: "Design",
-            },
-            {
-              id: 13,
-              title: "Add discount code to checkout page",
-              date: "Sep 14",
-              type: "Feature Request",
-            },
-          ],
-        },
-        {
-          title: "FINALIZADO",
-          tasks: [
-            {
-              id: 14,
-              title: "Add discount code to checkout page",
-              date: "Sep 14",
-              type: "Feature Request",
-            },
-            {
-              id: 15,
-              title: "Design shopping cart dropdown",
-              date: "Sep 9",
-              type: "Design",
-            },
-            {
-              id: 16,
-              title: "Add discount code to checkout page",
-              date: "Sep 14",
-              type: "Feature Request",
-            },
-          ],
-        },
-      ],
-    };
-  },
+  data: () => ({
+    dialog: false,
+    historias: [
+      {
+        id: 1,
+        idUsuarioAsignado: 1,
+        nombre: "Plans 2015",
+        fase: "TO DO",
+      },
+    ],
+    usuarios: [
+      {
+        idUsuario: 1,
+        nombre: "John",
+        apellido: "Heart",
+      },
+    ],
+
+    usuariosMap: {},
+    lists: [],
+    statuses: ["TO DO", "DOING", "DONE"],
+    indexAnterior: null,
+    indexActual: null,
+    historiaActual: {},
+  }),
 
   methods: {
-    onDragEnd() {},
-    onStart() {},
+    onListReorder(e) {
+      const list = this.lists.splice(e.fromIndex, 1)[0];
+      this.lists.splice(e.toIndex, 0, list);
+
+      const status = this.statuses.splice(e.fromIndex, 1)[0];
+      this.statuses.splice(e.toIndex, 0, status);
+    },
+    onTaskDragStart(e, listIndex) {
+      this.indexActual = listIndex;
+      e.itemData = e.fromData[e.fromIndex];
+    },
+    onTaskDrop(e, listIndex) {
+      this.indexAnterior = this.indexActual;
+      this.indexActual = listIndex;
+      e.fromData.splice(e.fromIndex, 1);
+      e.toData.splice(e.toIndex, 0, e.itemData);
+      this.historiaActual = e.itemData;
+      this.dialog = true;
+    },
+    actualizarHistoria() {
+      console.log(this.historiaActual);
+      if (this.indexActual % 2 == 0) {
+        this.revertirCambio();
+      }
+      this.dialog = false;
+    },
+    revertirCambio() {
+      let item = this.lists[this.indexAnterior];
+      this.lists[this.indexAnterior] = this.lists[this.indexActual];
+      this.lists[this.indexActual] = item;
+    },
+
+    getStatus(historia, estado) {
+      let status = new Map();
+      status.set("TO DO", "status-1");
+      status.set("DOING", "status-2");
+      status.set("DONE", "status-3");
+
+      return status.get(estado.toUpperCase());
+    },
+
+    actualizarKanban() {
+      this.statuses.forEach((status) => {
+        this.lists.push(
+          this.historias.filter(
+            (historia) => historia.fase.toUpperCase() === status.toUpperCase()
+          )
+        );
+      });
+    },
+  },
+
+  mounted() {
+    this.usuarios.forEach((usuario) => {
+      this.usuariosMap[usuario.idUsuario] = usuario.nombre;
+    });
+    this.actualizarKanban();
   },
 };
--- agile-sys-front/src/views/ModuloDesarrollo/Board/Board.vue
+++ agile-sys-front/src/views/ModuloDesarrollo/Board/Board.vue
@@ -1,36 +1,79 @@
 <template>
   <div>
-    <v-row class="flex justify-center">
-      <div
-        v-for="column in columns"
-        :key="column.title"
-        class="bg-gray rounded-lg px-3 py-3 column-width rounded mr-4"
-      >
-        <p class="text-gray font-weight-medium caption">{{ column.title }}</p>
-        <!-- Draggable component comes from vuedraggable. It provides drag & drop functionality -->
-        <draggable
-          :list="column.tasks"
-          :animation="200"
-          ghost-class="ghost-card"
-          group="tasks"
-          class="cursorMove"
-          @end="onDragEnd"
-          @start="onStart"
+    <br />
+    <v-row justify="center">
+      <div id="kanban">
+        <DxScrollView
+          class="scrollable-board"
+          direction="horizontal"
+          show-scrollbar="always"
         >
-          <!-- Each element from here will be draggable and animated. Note :key is very important here to be unique both for draggable and animations to be smooth & consistent. -->
-          <task-card
-            v-for="task in column.tasks"
-            :key="task.id"
-            :task="task"
-            class="mt-3"
-          ></task-card>
-          <!-- </transition-group> -->
-        </draggable>
+          <DxSortable
+            class="sortable-lists"
+            item-orientation="horizontal"
+            handle=".list-title"
+            @reorder="onListReorder"
+          >
+            <div
+              v-for="(list, listIndex) in lists"
+              :key="statuses[listIndex]"
+              class="list"
+            >
+              <div class="list-title dx-theme-text-color">
+                {{ statuses[listIndex] }}
+              </div>
+              <DxScrollView class="scrollable-list" show-scrollbar="always">
+                <DxSortable
+                  :data="list"
+                  class="sortable-cards"
+                  group="tasksGroup"
+                  @drag-start="onTaskDragStart($event, listIndex)"
+                  @add="onTaskDrop($event, listIndex)"
+                >
+                  <div
+                    v-for="historia in list"
+                    :key="historia.id"
+                    class="
+                      card
+                      dx-card dx-theme-text-color dx-theme-background-color
+                    "
+                  >
+                    <div
+                      :class="[
+                        'card-status',
+                        getStatus(historia, statuses[listIndex]),
+                      ]"
+                    />
+                    <div class="card-subject">{{ historia.nombre }}</div>
+                    <div class="card-assignee">
+                      {{ usuariosMap[historia.idUsuarioAsignado] }}
+                    </div>
+                  </div>
+                </DxSortable>
+              </DxScrollView>
+            </div>
+          </DxSortable>
+        </DxScrollView>
       </div>
     </v-row>
+
+    <v-dialog v-model="dialog" persistent width="560">
+      <v-card>
+        <v-card-title class="headline"
+          >Está seguro que desea eliminar este elemento?</v-card-title
+        >
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="actualizarHistoria">Aceptar</v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false"
+            >Cancelar</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script src="./Board.js"></script>
-
-<style scoped src="./Board.css"></style>
+<style src="./Board.css">
+</style>
